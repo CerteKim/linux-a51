9edde7358cfa73f1b783ec3377b9be74b38a2120
CerteKim <18364439+CerteKim@users.noreply.github.com>
2026-07-14 15:41:12 +0800

    try fix fifo=dddd1311

--- drivers/gpu/drm/panel/panel-himax-hx83121a.c
+++ drivers/gpu/drm/panel/panel-himax-hx83121a.c
@@ -278,8 +278,7 @@ static int hx83121a_probe(struct mipi_dsi_device *dsi)
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO |
-			  MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
-			  MIPI_DSI_MODE_VIDEO_HSE;
+			  MIPI_DSI_MODE_VIDEO_SYNC_PULSE;
 
 	hx83121a_init_dsc(ctx);
 	dsi->dsc = &ctx->dsc;
